181b0d3aac05101ae2cdb34f45ebc3468cec9237
abullock2017 <abullock2017@gmail.com>
2017-12-03 19:28:17 -0500

    Game Board update
    Changed the gameboard rooms and hallways from labels to buttons. And added buttons for leave game, options, make a suggestion and make an accusation.

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ Created on Nov 5, 2017
 
 import sys
 import socket
+import ipgetter
 from PyQt5 import QtWidgets, QtMultimedia, QtCore
 from PyQt5.QtWidgets import *
 from ClueLess import Ui_ClueLess
+from SuggestionDialog import Ui_SuggestionDialog
 import webbrowser
 
 from _thread import *
@@ -17,12 +19,6 @@ from servernet import ServerNet
 from clientnet import ClientNet
 
 port = 12346
-
-'''
-from mainMenuWindow import Ui_MainMenuWindow
-from gameLobbyMenuWindow import Ui_GameLobbyWindow
-from mainOptionsMenuWindow import Ui_MainOptionsMenuWindow
-'''
 
 class Main(QWidget, Ui_ClueLess):
     
@@ -35,12 +31,12 @@ class Main(QWidget, Ui_ClueLess):
         self.ui.gameLobbyButton.clicked.connect(self.displayGameLobbyMenu)
         self.ui.mainOptionsButton.clicked.connect(self.mainOptionsButton_Clicked)
         self.ui.instructionsButton.clicked.connect(self.howToPlay)
-        
+		
     def howToPlay(self):
         webbrowser.open('https://www.hasbro.com/common/documents/dad2885d1c4311ddbd0b0800200c9a66/2BFAEC9E5056900B102C3859E9AC6332.pdf')
     
     def changeVolume(self):
-        musicPlayer.setVolume( self.ui.mainMusicSlider.value() )
+        musicPlayer.setVolume(self.ui.mainMusicSlider.value())
     
     def goBack(self, hideMenu, showMenu):
         hideMenu.hide()
@@ -58,8 +54,7 @@ class Main(QWidget, Ui_ClueLess):
         self.ui.stackedWidget.setCurrentIndex(2)
         self.ui.gameLobbyMenu.hide()
         self.ui.createGameMenu.show()
-        hostName = socket.gethostname()
-        ipAddress = socket.gethostbyname(hostName)
+        ipAddress = ipgetter.myip()
         self.ui.ipAddressLabel.setText(ipAddress)
         self.ui.createButton.clicked.connect(lambda: self.displayGameLobby("gameHost"))
         self.ui.cancelButton.clicked.connect(lambda: self.goBack(self.ui.createGameMenu, self.ui.gameLobbyMenu))
@@ -72,28 +67,22 @@ class Main(QWidget, Ui_ClueLess):
         self.ui.cancelJoinButton.clicked.connect(lambda: self.goBack(self.ui.joinGameMenu, self.ui.gameLobbyMenu))
         
     def displayGameLobby(self, playerType):
-        
-        self.ui.playerThreeNameSlot.setText( "Person" )
-        
         if playerType == "gameHost":
             self.ui.playerOneNameSlot.setText(self.ui.gameHostName.text())
             
-            startsocket = ServerNet(port)   #Instantiate server class
-            threading.Thread(target = startsocket.listen).start() #Server is now listening for connection requests
+            startsocket = ServerNet(port) #Instantiate server class
+            threading.Thread(target = startsocket.listen).start() # Server is now listening
             print("server created")
-            
+			
             '''
-            while True:
-                massmsg = input("MSG to all clients: ")   # Need to verify valid IP address?
-                startsocket.sendmsgtoclients(massmsg)
-            '''
-            
+			while True:
+				massmsg = input("MSG to all clients: ") # Need to verify valid IP address?
+				startsocket.sendmsgtoclients(massmsg)
+			'''
         else:
-            
-            
-            #host = input("Please enter IP address: ")   #This get server IP from user
+			#host = input("Please enter IP address: ") #This get server IP from user
             host = self.ui.gameIPInput.text()
-            clientconnection = ClientNet(host)          #Established socket to server
+            clientconnection = ClientNet(host) #Established socket to server
             print("client created")
             clientconnection.sendmsg("hello")
             
@@ -104,7 +93,6 @@ class Main(QWidget, Ui_ClueLess):
                     break
                 clientconnection.sendmsg(message)
             '''
-            
             if self.ui.playerTwoNameSlot.text() == "Waiting...":
                 self.ui.playerTwoNameSlot.setText(self.ui.newPlayerName.text())
             elif self.ui.playerThreeNameSlot.text() == "Waiting...":
@@ -133,30 +121,36 @@ class Main(QWidget, Ui_ClueLess):
         self.ui.gameLobby.hide()
         self.ui.characterSelection.show()
         self.ui.continueButton.clicked.connect(self.displayGameBoard)
-    
+        
     def displayGameBoard(self):
         self.ui.stackedWidget.setCurrentIndex(6)
         self.ui.characterSelection.hide()
         self.ui.gameBoard.show()
+        self.ui.makeSuggestionButton.clicked.connect(self.makeSuggestion)
+	
+    def makeSuggestion(self):
+        self.suggestion = Ui_SuggestionDialog()()
+        self.suggestion.setupUi(self)
+        '''Need to research how to display dialog window'''
     
     def mainOptionsButton_Clicked(self):
         self.ui.stackedWidget.setCurrentIndex(7)
         self.ui.mainMenu.hide()
         self.ui.mainOptionsMenu.show()
         self.ui.mainOptionsBack.clicked.connect(lambda: self.goBack(self.ui.mainOptionsMenu, self.ui.mainMenu))
-       
+        
     def closeEvent(self, *args, **kwargs):
         startsocket.terminate()
         clientconnection.terminate()
         return QWidget.closeEvent(self, *args, **kwargs)
-    
+		
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     musicPlayer = QtMultimedia.QMediaPlayer()
     musicSound = QtMultimedia.QMediaContent(QtCore.QUrl.fromLocalFile("clue_music.wma"))
     musicPlayer.setMedia(musicSound)
     musicPlayer.setVolume(100)
-    musicPlayer.play()  
+    musicPlayer.play()
     window = Main()
-    window.show()    
+    window.show()
     sys.exit(app.exec_())
